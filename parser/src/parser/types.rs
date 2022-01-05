95ffed6c12b2ed51cd83871cd0d108a0f8628876
Meqolo <iamanubbert@gmail.com>
2022-01-05 18:08:00 +0000

    Add support for numeric types

--- parser/src/parser/types.rs
+++ parser/src/parser/types.rs
@@ -7,9 +7,9 @@ pub trait Types<'a> {
 impl<'a> Types<'a> for Parser<'a> {
     fn transform_type(&mut self, type_string: String) -> &'a str {
         match type_string.as_str() {
-            "str" | "String" => {
-                return "string";
-            }
+            "str" | "String" => "string",
+            "u8" | "u16" | "u32" | "u64" | "u128" | "usize" | "i8" | "i16" | "i32" | "i64"
+            | "i128" | "isize" | "f32" | "f64" => "number",
             _ => return "",
         }
     }
